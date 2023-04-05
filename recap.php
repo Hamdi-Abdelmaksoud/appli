127b7b5bd99e59868fdb7b1eb17ef16a811fd256
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2023-04-05 14:52:01 +0100

    mettre le style pour recap.php

--- recap.php
+++ recap.php
@@ -9,6 +9,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Récapitulatif des produits</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
@@ -17,41 +18,43 @@ session_start();
     if (!isset($_SESSION['products']) || empty($_SESSION['products'])) {
         echo "<p>Aucun produit en session ...</p>";
     } else {
-        echo "<table class='table'",
+        echo "<table class='table table-bordered table-dark m-2' >",
         "<thead>",
         "<tr>",
-        "<th>qtt-</th>",
-        "<th 'scope=col'>#</th>",
-        "<th 'scope=col'>Nom</th>",
-        "<th 'scope=col'>Prix</th>",
-        "<th 'scope=col'>Quantité</th>",
-        "<th 'scope=col'>Total</th>",
-        "<th>qtt+</th>",
+        //  "<th scope='col'>qtt-</th>",
+        "<th scope='col'>#</th>",
+        "<th scope='col'>Nom</th>",
+        "<th scope='col'>Prix</th>",
+        "<th scope='col'>Quantité</th>",
+        "<th scope='col'>Total</th>",
+        // "<th scope='col'>qtt+</th>",
         "</tr>",
         "</thead>",
         "<tbody>";
         $totalGeneral = 0;
         foreach ($_SESSION['products'] as $index => $product) {
         
-            echo "<tr>",
-            "<td> <a href='traitement.php?action=down-qtt&index=$index'><i class='fa-regular fa-square-minus'></i></button></a></td>",
-            "<td 'row'>" . $index . "</td>",
+            echo "<tr scope='row'>",
+           
+            "<td >" ."<a href='traitement.php?action=delete&index=$index'><i class='fa-solid fa-circle-xmark'></i></a>" . "</td>",
             "<td>" . $product['name'] . "</td>",
             "<td>" . number_format($product['price'], 2, ",", "&nbsp;") . "&nbsp;€</td>",
-            "<td>" . $product['qtt'] . "</td>",
+            "<td>" ."<a href='traitement.php?action=down-qtt&index=$index'><i class='fa-solid fa-circle-minus'></i></a>  ". $product['qtt'] ."   <a href='traitement.php?action=up-qtt&index=$index'><i class='fa-solid fa-circle-plus'></i></a> " ."</td>",
             "<td>" . number_format($product['total'], 2, ",", "&nbsp;") . "&nbsp;€</td>",
-            "<td> <a href='traitement.php?action=up-qtt&index=$index'><button><i class='fa-solid fa-plus'></i></button></a></td>",
+        
             "</tr>";
             $totalGeneral += $product['total'];
         }
-        echo "<tr>",
-        "<td colspan=4>Total général : </td>",
+        echo "<tr scope='row'>",
+        "<td><a href='traitement.php?action=clear'><i class='fa-solid fa-trash'></i>vider <a></td>",
+        "<td colspan=3>Total général : </td>",
         "<td><strong>" . number_format($totalGeneral, 2, ".", "&nbsp") . "&nbsp;€</strong></td>",
+        
         "</tr>",
         "</tbody>",
         "</table>";
     } ?>
-    <a href="index.php">Ajouter un produit</a> 
+    <a href="index.php"><button class="btn btn-primary">Ajouter des produits</button></a> 
 
 </body>
 
--- traitement.php
+++ traitement.php
@@ -1,17 +1,17 @@
 <?php
 session_start();
-if (isset($_GET['action'])) {
+if (isset($_GET['action'])) 
+{
     switch ($_GET['action']) {
             //*____________________________Ajouter un produit________________________________
         case "add":
-            if (isset($_POST['submit']))
+            if (isset($_POST['submit'])) 
             {
                 $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
                 $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
                 $qtt = filter_input(INPUT_POST, 'qtt', FILTER_VALIDATE_INT);
 
-                if ($name && $price && $qtt)
-                {
+                if ($name && $price && $qtt) {
                     $product = [
                         "name" => $name,
                         "price" => $price,
@@ -22,41 +22,37 @@ if (isset($_GET['action'])) {
                     $_SESSION['message'] = "le produit " . $name . " a bien été ajouté";
                 }
             }
+            header("Location:index.php");
             break;
-        
+
             //*__________________________supprimer produit____________________________________
-            case "delete":    
-                unset($_SESSION['products'][$_GET['index']]); // On supprime un produit
-                break;
-                 
+        case "delete":
+            unset($_SESSION['products'][$_GET['index']]); // On supprime un produit
+            header("Location:recap.php");
+            break;
+
             //*__________________________vider le panier____________________________________
-                case "clear":
-                    unset($_SESSION['products']); // On supprime un produit
-                 
-                    break;
-                
-             //*__________________________augmenter la quantité d'un produit____________________________________
-                case "up-qtt":
-                $_SESSION['products'][$_GET['index']]['qtt']++;
-                $_SESSION['products'][$_GET['index']]['total']+=$_SESSION['products'][$_GET['index']]['price'];
-                header("Location:recap.php");
-                break;
-              //*__________________________ diminuer la quantité d'un produit____________________________________
-              case "down-qtt":
-                if($_SESSION['products'][$_GET['index']]['qtt']>1)
-                {
-                   $_SESSION['products'][$_GET['index']]['qtt']--;
-                   $_SESSION['products'][$_GET['index']]['total']-=$_SESSION['products'][$_GET['index']]['price'];
-                   header("Location:recap.php");
-                }
-                else
-                {
-                    unset($_SESSION['products'][$_GET['index']]);
-                }
-                header("Location:recap.php");
-                break;
-    } 
+        case "clear":
+            unset($_SESSION['products']); // On supprime un produit
+            header("Location:index.php");
+            break;
 
+            //*__________________________augmenter la quantité d'un produit____________________________________
+        case "up-qtt":
+            $_SESSION['products'][$_GET['index']]['qtt']++;
+            $_SESSION['products'][$_GET['index']]['total'] += $_SESSION['products'][$_GET['index']]['price'];
+            header("Location:recap.php");
+            break;
+            //*__________________________ diminuer la quantité d'un produit____________________________________
+        case "down-qtt":
+            if ($_SESSION['products'][$_GET['index']]['qtt'] > 1) {
+                $_SESSION['products'][$_GET['index']]['qtt']--;
+                $_SESSION['products'][$_GET['index']]['total'] -= $_SESSION['products'][$_GET['index']]['price'];
+                header("Location:recap.php");
+            } else {
+                unset($_SESSION['products'][$_GET['index']]);
+            }
+            header("Location:recap.php");
+            break;
+    }
 }
-
-
